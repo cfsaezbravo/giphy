592ad17ec25c3d1cd8a88aae58fc03ad3d93908b
Camila Sáez <cfsaezbravo@gmail.com>
2017-07-04 15:57:38 -0400

    Second commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-main.js
+(document).ready(function(){
+	var dibujarGifs = function(data){
+		var gif = "";
+		var url = "";
+		data.forEach(function(element) {
+			gif = element.images.downsized_large.url;
+			url = element.bitly_gif_url;
+			$("#elementos").append(armarTemplate(gif, url));
+		});
+	}
+
+	var armarTemplate = function(gif, url){
+		var t = "<div class='elemento'><img src='" + gif + "'/><a href='" + url + "'>Ver más</a></div>"
+		return t;
+	}
+})
